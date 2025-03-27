a012de64d1585d9184eb4c0f9df4686ba3763dd7
javierng2knus <javierng2knus@gmail.com>
2025-03-28 06:27:02 +0800

    Start Menus
    Need to check if working

--- Assets/Scripts/AppCoreManagers/SingletonPattern.cs
+++ Assets/Scripts/AppCoreManagers/SingletonPattern.cs
@@ -5,28 +5,17 @@ using System.Collections;
 public abstract class SingletonPattern<T> : MonoBehaviour
     where T : Component
 {
-    private static T _instance;
-    public static T Instance
+    public static T Instance { get; private set; }
+
+    public virtual void Awake()
     {
-        get
+        if (Instance == null)
         {
-            if (_instance == null)
-            {
-                var objs = FindAnyObjectByType(typeof(T)) as T[];
-                if (objs.Length > 0)
-                    _instance = objs[0];
-                if (objs.Length > 1)
-                {
-                    Debug.LogError("There is more than one " + typeof(T).Name + " in the scene.");
-                }
-                if (_instance == null)
-                {
-                    GameObject obj = new GameObject();
-                    obj.hideFlags = HideFlags.HideAndDontSave;
-                    _instance = obj.AddComponent<T>();
-                }
-            }
-            return _instance;
+            Instance = this as T;
+        }
+        else
+        {
+            Destroy(gameObject);
         }
     }
 }
